c351c0ec715ed674167fee359a5950f169b1cca8
Pablo Spata <pablospata@hotmail.com>
2022-06-26 17:13:08 -0300

    Mas metodos y comentarios AGREGADOS

--- tp3-consigna1/src/tp3/consigna1/ListaDobleEnlazada.java
+++ tp3-consigna1/src/tp3/consigna1/ListaDobleEnlazada.java
@@ -2,65 +2,248 @@ package tp3.consigna1;
 
 public class ListaDobleEnlazada {
     
+    /*    
+    Estos nodos representan el head y el tail de la lista.
+    Y el atributo longitud se incrementa o decrece a medida que se insertan o
+    eliminan elementos
+    */
     Nodo cabeza;
     Nodo cola;
+    private int longitud;
 
+    /**
+     * Construir lista doblemente enlazada
+     */
     public ListaDobleEnlazada() {
         cabeza = new Nodo(null);
         cola = new Nodo(null);        
     }
     
+    /**
+     * Insertar producto en forma ordenada
+     * @param p Producto que se desea insertar   
+     */
     public void insertarOrdenado(Producto p) {
-        Nodo nuevo = new Nodo(p);
+        Nodo nuevo = new Nodo(p);        
         
+        /*                
+        NOTA SOBRE LA IMPLEMENTACION:
+        Seguimos las indicaciones del libro "Estructuras de datos en Java" de 
+        Mark A. Weiss que recomienda crear un nodo extra Head y otro Tail que no almacenen
+        ningun dato, pero que sirven para apuntar a los nodos, de modo que todo nodo tiene
+        uno que lo apunte y no hacerle tratamientos especiales a estos (Pag. 609, Seccion
+        17.1.1 'Nodos de cabecera')         
+        
+        */
         if(cabeza.siguiente == null) {
+        //Primer comprobacion para ver si la lista esta vacia, si true asignamos directamente el primer
+        //nodo
             
             cabeza.siguiente = nuevo;            
             cola.anterior = nuevo;
             nuevo.siguiente = cola;
             nuevo.anterior = cabeza;
+            
+            longitud++;
         }
         else {
             
+            //Como ya existen nodos en la lista utilizamos un bucle para recorrerla.
+            //Recorremos la lista mientras el codigo a ingresar sea mayor a cada uno
+            //de los nodos y no se llegue al final de la lista.
             Nodo aux = cabeza.siguiente;
             while(nuevo.elemento.codigo > aux.elemento.codigo && aux.siguiente.elemento != null) {
                 
-                aux = aux.siguiente;
+                aux = aux.siguiente;                                
             }
             
+            //Una vez que se sale del bucle si ya existe nodo con el mismo codigo
+            //No se inserta y se lanza un mensaje de error.
             if(nuevo.elemento.codigo == aux.elemento.codigo) {
                 
-                System.out.println("\nYa existe un producto con el mismo codigo");                
+                System.out.println("\nERROR. Ya existe un producto con el mismo codigo");                
             }
             else if(nuevo.elemento.codigo > aux.elemento.codigo) { 
                 
-                nuevo.siguiente = cola;
-                nuevo.anterior = aux;
+                //Aca el codigo es mayor al ultimo codigo de la lista, por lo tanto se agrega 
+                //como ultimo nodo.
                 
-                aux.siguiente = nuevo;                
+                cola.anterior.siguiente = nuevo;
+                nuevo.anterior = cola.anterior;
+                nuevo.siguiente = cola;
+                cola.anterior = nuevo;        
+
+                longitud++;
             }
             else {
+                
+                //En este caso el codigo quedo entre medio, por lo que se hace las correspondientes
+                // asignaciones
                 
                 nuevo.siguiente = aux;
                 nuevo.anterior = aux.anterior;
                 
                 aux.anterior.siguiente = nuevo;
                 aux.anterior = nuevo;                                
+                
+                longitud++;
             }
         }
     }
     
+    /**
+     * Insertar producto al principio de la lista
+     * @param p Producto que se desea insertar        
+     */
+    public void insertarInicio(Producto p) {
+        Nodo nuevo = new Nodo(p);
+        
+        cabeza.siguiente.anterior = nuevo;
+        nuevo.siguiente = cabeza.siguiente;
+        nuevo.anterior = cabeza;
+        cabeza.siguiente = nuevo;
+        
+        longitud++;
+    }
+    
+    /**
+     * Insertar producto al final de la lista
+     * @param p Producto que se desea insertar        
+     */    
+    public void insertarFin(Producto p) {
+        Nodo nuevo = new Nodo(p);
+        
+        cola.anterior.siguiente = nuevo;
+        nuevo.anterior = cola.anterior;
+        nuevo.siguiente = cola;
+        cola.anterior = nuevo;        
+        
+        longitud++;
+    }
+    
+    /**
+     * Insertar producto N posicion de la lista
+     * @param p Producto que se desea insertar        
+     * @param n Posicion de la lista donde se insertara p        
+     */    
+    public void insertarPosicion(Producto p, int n) {
+        
+        if(n < longitud) {
+            
+            Nodo nuevo = new Nodo(p);        
+            Nodo aux = cabeza.siguiente;
+
+            for(int i = 0; i < n; i++)    
+                aux = aux.siguiente;                    
+
+            aux.anterior.siguiente = nuevo;
+            nuevo.anterior = aux.anterior;
+            nuevo.siguiente = aux;
+            aux.anterior = nuevo;
+            
+            longitud++;
+        }
+        else
+            System.out.println("\nLa lista no posee la posicion indicada.");
+    }
+    
+    /**
+     * Eliminar producto del principio de la lista  
+     */    
+    public void eliminarInicio() {
+        if(!estaVacia()) {
+            cabeza.siguiente.siguiente.anterior = cabeza;
+            cabeza.siguiente = cabeza.siguiente.siguiente;
+
+            longitud--;
+        }
+        else
+            System.out.println("\nNo se puede eliminar. La lista esta vacia.");
+    }
+    
+    /**
+     * Eliminar producto del final de la lista  
+     */        
+    public void eliminarFin() {
+        if(!estaVacia()) {            
+            cola.anterior.anterior.siguiente = cola;
+            cola.anterior = cola.anterior.anterior;
+            
+            longitud--;
+        }
+        else
+            System.out.println("\nNo se puede eliminar. La lista esta vacia.");
+    }
+    
+    /**
+     * Eliminar producto de la N posicion de la lista
+     * @param n Posicion de la lista donde se eliminara p        
+     */
+    public void eliminarPosicion(int n) {
+        
+        if(!estaVacia()) {            
+            
+            if(n < longitud) {
+
+                Nodo aux = cabeza.siguiente;
+
+                for(int i = 0; i < n; i++)    
+                    aux = aux.siguiente;                    
+
+                aux.anterior.siguiente = aux.siguiente;
+                aux.siguiente.anterior = aux.anterior;
+                                
+                longitud--;
+            }
+            else
+                System.out.println("\nLa lista no posee la posicion indicada.");
+        }
+        else
+            System.out.println("\nNo se puede eliminar. La lista esta vacia.");
+    }
+    
+    /**
+    * Comprobar si la lista esta logicamente vacia.
+    * @return true si esta vacia, false en caso contrario
+    */
+    public boolean estaVacia() {
+        return cabeza.siguiente == null;
+    }
+    
+    /**
+    * Hacer que la lista este logicamente vacia.
+    */
+    public void vaciar() {
+        cabeza.siguiente = null;
+        cola.anterior = null;
+        longitud = 0;
+    }
+    
+    /**
+     * Devuele el la logitud actual de la lista.
+     */
+    public int obtenerLongitud() {
+        return longitud;
+    }
+        
+    /**
+     * Metodo para imprimir todos los elementos de la lista.
+     */
     public void imprimirLista() {
         
         Nodo aux = cabeza.siguiente;
         
-        System.out.println("\n\n--- Imprimiendo elementos ---");        
-        while(aux.elemento != null)
-        {            
-            System.out.println("\nCodigo: " + aux.elemento.codigo);
-            System.out.println("Descripcion: " + aux.elemento.descripcion);
-            System.out.println("Precio: " + aux.elemento.precio);
-            aux = aux.siguiente;
+        if(!estaVacia()) {
+            System.out.println("\n\n--- Imprimiendo elementos ---");        
+            while(aux.elemento != null)
+            {            
+                System.out.println("\nCodigo: " + aux.elemento.codigo);
+                System.out.println("Descripcion: " + aux.elemento.descripcion);
+                System.out.println("Precio: " + aux.elemento.precio);
+                aux = aux.siguiente;
+            }
         }
+        else    
+            System.out.println("\n\n--- No hay elementos para imprimir---");        
     }
 }
--- tp3-consigna1/src/tp3/consigna1/Tp3Consigna1.java
+++ tp3-consigna1/src/tp3/consigna1/Tp3Consigna1.java
@@ -4,16 +4,17 @@ public class Tp3Consigna1 {
 
     public static void main(String[] args) {
 
+        //Probando clase Lista y los metodos solicitados en la consigna.
         ListaDobleEnlazada listaTest = new ListaDobleEnlazada();        
         Producto p;
         
         p = new Producto(10, "Silla", 40.5);        
         listaTest.insertarOrdenado(p);        
-        listaTest.imprimirLista();
+        listaTest.imprimirLista();        
         
         p = new Producto(2, "Mesa", 300);                
         listaTest.insertarOrdenado(p);
-        listaTest.imprimirLista();
+        listaTest.imprimirLista();  
                         
         p = new Producto(5, "Heladera", 550);                
         listaTest.insertarOrdenado(p);
@@ -22,9 +23,61 @@ public class Tp3Consigna1 {
         p = new Producto(5, "Sillon", 300);                
         listaTest.insertarOrdenado(p);
         listaTest.imprimirLista();        
-        
+                       
         p = new Producto(15, "Televisor", 500);                
         listaTest.insertarOrdenado(p);
-        listaTest.imprimirLista();
+        listaTest.imprimirLista();        
+        
+        System.out.println("\nLongitud actual de la Lista: " + listaTest.obtenerLongitud());               
     }    
 }
+
+/*
+    CODIGO PARA PROBAR EL RESTO DE METODOS DE LA LISTA DOBLEMENTE ENLAZADA:
+
+        //Probando metodos extras
+        p = new Producto(13, "Cuadro", 33);                
+        listaTest.insertarInicio(p);
+        listaTest.imprimirLista();
+                
+        p = new Producto(7, "Plato", 8);                
+        listaTest.insertarFin(p);
+        listaTest.imprimirLista();
+        
+        p = new Producto(123, "Cuchara", 4);                
+        listaTest.insertarFin(p);
+        listaTest.imprimirLista();
+        
+        p = new Producto(23, "Vaso", 4);                
+        listaTest.insertarPosicion(p, 4);
+        listaTest.imprimirLista();
+        
+        System.out.println("\nLongitud actual de la Lista: " + listaTest.obtenerLongitud());
+        
+        listaTest.eliminarInicio();        
+        listaTest.imprimirLista();
+        
+        System.out.println("\nLongitud actual de la Lista: " + listaTest.obtenerLongitud());
+        
+        listaTest.eliminarFin();        
+        listaTest.imprimirLista();
+        
+        System.out.println("\nLongitud actual de la Lista: " + listaTest.obtenerLongitud());
+        
+        listaTest.eliminarPosicion(4);                
+        listaTest.eliminarPosicion(2);        
+        listaTest.imprimirLista();
+        
+        System.out.println("\nLongitud actual de la Lista: " + listaTest.obtenerLongitud());
+        
+        System.out.println("\nEsta vacia: " + listaTest.estaVacia());        
+        
+        System.out.println("\nLongitud actual de la Lista: " + listaTest.obtenerLongitud());
+        System.out.println("\n\nVaciando lista...");
+        listaTest.vaciar();
+        
+        listaTest.imprimirLista();
+        System.out.println("\nLongitud actual de la Lista: " + listaTest.obtenerLongitud());
+        
+        System.out.println("\nEsta vacia: " + listaTest.estaVacia());        
+*/
