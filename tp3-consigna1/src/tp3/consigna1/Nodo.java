bed99b8b224fac205183576a82dc15a329958f48
Pablo Spata <pablospata@hotmail.com>
2022-06-27 19:08:15 -0300

    Comentarios agregados para presentar

--- tp3-consigna1/src/tp3/consigna1/Nodo.java
+++ tp3-consigna1/src/tp3/consigna1/Nodo.java
@@ -1,3 +1,12 @@
+/* INTEGRANTES DEL GRUPO:
+Spata, Pablo Leonardo, Legajo: VINF10171
+Castillo, Francisco, Legajo: VINF013417
+Ruiz, Gonzalo Mario, Legajo: VINF011900
+Paris, Bruno Jabie, Legajo: VINF012105
+Maria Gabriela Mauri, Legajo: VINF011820
+
+Fecha: 27/06/2022
+*/
 package tp3.consigna1;
 
 public class Nodo {
